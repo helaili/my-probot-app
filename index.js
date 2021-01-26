1fcc908bb7b9666e449adf0a3c6fed36521b374a
Alain Hélaïli <alain@carbonhub.io>
2021-01-26 22:21:40 +0100

    it works

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
-
+  
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
